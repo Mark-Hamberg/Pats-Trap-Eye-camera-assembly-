4d456786baa9999bdd037a9ce14cae6eeb293519
Cr33pslayer <66366172+Cr33pslayer@users.noreply.github.com>
2025-01-10 16:19:02 +0100

    Update Arduino_Main_Code.ino

--- Arduino_Main_Code/Arduino_Main_Code.ino
+++ Arduino_Main_Code/Arduino_Main_Code.ino
@@ -1,16 +1,29 @@
 #include <Servo.h>
+#include <AccelStepper.h>
+
+// Define the pins for your stepper motor
+#define X_STEP_PIN 5
+#define X_DIR_PIN 2
+#define X_ENABLE_PIN 8
 
 int analogPin = A0; // potentiometer read the Robot arm value
 float val = 0;  // variable to store the value read
 
 //Pins actuator
-int pinAct1In = 12;
-int pinAct1Out = 13;
-int pinAct2In = 10;
-int pinAct2Out = 11;
+int pinAct2In = 10;     //Actuator1
+int pinAct2Out = 11;    //Actuator1
+int pinAct1In = 12;     //Actuator2
+int pinAct1Out = 13;    //Actuator2
 
 //Servo Camera Gripper
 Servo myservo;
+
+// Create an instance of AccelStepper with DRIVER mode (for step/direction control)
+AccelStepper stepper(AccelStepper::DRIVER, X_STEP_PIN, X_DIR_PIN);
+
+String fp_num = "focus_9";
+uint64_t last_message_time = 0;
+
 
 void setup() {
   Serial.begin(9600);           //  setup serial
@@ -32,6 +45,20 @@ void ServoCamera(int Position){ //0 degree: open  //90 degree: close
 
 void ExtendAct(int actuator)
 {
+  Serial.print("extend");
+  if (actuator==1){ // actuator 1 is opening actuator
+    digitalWrite(pinAct1In, HIGH);
+    digitalWrite(pinAct1Out, LOW);
+    delay(8000);      }
+  else {
+    digitalWrite(pinAct2In, HIGH);
+    digitalWrite(pinAct2Out, LOW);
+    delay(4000);      }
+}
+
+void RetractAct(int actuator)
+{
+  Serial.print("retract");
   if (actuator==1){ // actuator 1 is opening actuator
     digitalWrite(pinAct1In, LOW);
     digitalWrite(pinAct1Out, HIGH);
@@ -42,47 +69,62 @@ void ExtendAct(int actuator)
     delay(4000);    }
 }
 
-void RetractAct(int actuator)
-{
-  if (actuator==1){ // actuator 1 is opening actuator
-    digitalWrite(pinAct2In, HIGH);
-    digitalWrite(pinAct2Out, LOW);
-    delay(8000);      }
-  else {
-    digitalWrite(pinAct2In, HIGH);
-    digitalWrite(pinAct2Out, LOW);
-    delay(4000);      }
-}
-
 void loop() {
+  /*
   val = (analogRead(analogPin) );  // read the input pin
-  val = val * (5.0 / 1023.0);
-  Serial.println(val);          // debug value
+  val = val * (5.0 / 1023.0);     
+  */
+
+  if (val == 0.0){
+    val = Serial.parseFloat();
+    Serial.print("Received value: ");
+    Serial.println(val);      // Print the value
+  //Serial.println(val);          // debug value
+  }  
 
   if (val >= 0.0 && val <= 0.4) { //Nothing
-  }
-  if (val >= 0.5 && val <= 1.0) { //Open Camera Gripper
+  } else if (val >= 0.5 && val <= 1.0) { //Open Camera Gripper
     ServoCamera(0);
-  }
-  if (val >= 1.1 && val <= 1.5) { //Close Camera Gripper
+  } else if (val >= 1.1 && val <= 1.5) { //Close Camera Gripper
     ServoCamera(90);
-  }
-  if (val >= 1.6 && val <= 2.0) { //Extend Actuator 1
+  } else if (val >= 1.6 && val <= 2.0) { //Extend Actuator 1
     ExtendAct(1);
-  }
-  if (val >= 2.1 && val <= 2.5) { //Retract Actuator 1
+  } else if (val >= 2.1 && val <= 2.5) { //Retract Actuator 1
     RetractAct(1);
-  }
-  if (val >= 2.6 && val <= 3.0) { //Extend Actuator 2
+  } else if (val >= 2.6 && val <= 3.0) { //Extend Actuator 2
     ExtendAct(2);
-  }
-  if (val >= 3.1 && val <= 3.5) { //Retract Actuator 2
+  } else if (val >= 3.1 && val <= 3.5) { //Retract Actuator 2
     RetractAct(2);
+  } else if (val >= 3.6 && val <= 4.0) { //Activate glue
+  
+   if (Serial.available() > 0) {
+        // Read the number of steps from the serial input
+        long steps = Serial.parseInt();
+        last_message_time = millis();
+        // Check if steps value is valid (not 0)
+        if (steps != 0) {
+            // Move the stepper by the given number of steps
+            stepper.move(steps);
+            stepper.runToPosition();  // Block until the move is completed
+
+            // Send a "Done" message to the PC after the movement is completed
+            delay(1);
+            Serial.println("Done");
+        if (steps == 0){Serial.println("Done");}    
+        }
+    }
+    if (millis() - last_message_time > 10 * 1000) {
+  // Serial.println("off");
+      digitalWrite(X_ENABLE_PIN, HIGH);
+        }
+
+
+
+
+  } else if (val >= 4.1 && val <= 4.5) {
+
+  } else if (val >= 4.6 && val <= 5.0) {
+
   }
-  if (val >= 3.6 && val <= 4.0) { //Activate glue
-  }
-  if (val >= 4.1 && val <= 4.5) {
-  }
-  if (val >= 4.6 && val <= 5.0) {
-  }
+  val = 0.0;
 }
