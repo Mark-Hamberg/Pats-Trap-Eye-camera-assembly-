5b6c9a7a71d279be7ca53731f83ec8bbc526c481
Cr33pslayer <66366172+Cr33pslayer@users.noreply.github.com>
2025-01-20 10:47:06 +0100

    Update Arduino_Main_Code.ino

--- Arduino_Main_Code/Arduino_Main_Code.ino
+++ Arduino_Main_Code/Arduino_Main_Code.ino
@@ -2,11 +2,11 @@
 #include <AccelStepper.h>
 
 // Define the pins for your stepper motor
-#define X_STEP_PIN 5
-#define X_DIR_PIN 2
+#define X_STEP_PIN 7
+#define X_DIR_PIN 4
 #define X_ENABLE_PIN 8
 
-
+/*######################################################### Initiate ################################################################*/
 int endswitchPin = 6;   // De pin waarop de eindschakelaar is aangesloten
 
 int analogPin = A0; // potentiometer read the Robot arm value
@@ -20,14 +20,14 @@ int pinAct1Out = 13;    //Actuator2
 
 //Servo Camera Gripper
 Servo myservo;
+int currentAngle = 0;   //current angle of the servomotor
 
 // Create an instance of AccelStepper with DRIVER mode (for step/direction control)
 AccelStepper stepper(AccelStepper::DRIVER, X_STEP_PIN, X_DIR_PIN);
-
 String fp_num = "focus_9";
 uint64_t last_message_time = 0;
 
-
+/*######################################################### Set-Up ################################################################*/
 void setup() {
   Serial.begin(9600);           //  setup serial
   
@@ -39,9 +39,11 @@ void setup() {
   pinMode(analogPin, INPUT);
   pinMode(endswitchPin, INPUT_PULLUP);
 
-
   myservo.attach(9); // Servo attach to pin 9
+  myservo.write(currentAngle);
 }
+
+/*######################################################### Functions ################################################################*/
 
 // void EndSwitch(){
 //   switchState = digitalRead(endswitchPin);
@@ -55,15 +57,21 @@ void setup() {
 //   delay(500);
 // }
 
-void ServoCameraClose(int Position){ //0 degree: open  //90 degree: close
-  for (int pos=30; pos <=89; pos++){
-    myservo.write(pos);
-    delay(15);
-  }
-}
+void ServoCamera(int newAngle){ //0 degree: open  //90 degree: close
+  int stepDelay = 20;       // The delay between steps(ms), if increased: slow down gripper
 
-void ServoCameraOpen(int Position){ //0 degree: open  //90 degree: close
-  myservo.write(30);
+  if (newAngle > currentAngle) {
+    for (int pos = currentAngle; pos <= newAngle; pos++) {
+      myservo.write(pos);
+      delay(stepDelay);
+    }
+  } else {
+    for (int pos = currentAngle; pos >= newAngle; pos--) {
+      myservo.write(pos);
+      //delay(stepDelay);
+    }
+  }
+  currentAngle = newAngle;  // Update de huidige positie
 }
 
 void ExtendAct(int actuator)
@@ -107,6 +115,25 @@ void StilstandAct(int actuator) {
   }
 }
 
+void GlueSation(long steps){  //full rotation of stepper motor is 1600 steps
+    if (steps != 0) {
+    last_message_time = millis();  // Register starttimeRegistreer starttijd van de beweging
+    
+    stepper.move(steps);           // Beweeg de steppermotor met het opgegeven aantal stappen
+    stepper.runToPosition();        // Wacht tot de beweging is voltooid
+
+    Serial.println("Done");         // Bevestiging dat de beweging klaar is
+  }
+
+  // Schakel de stepper uit als er 10 seconden geen beweging is geweest
+  if (millis() - last_message_time > 10 * 1000) {
+    digitalWrite(X_ENABLE_PIN, HIGH);  // Zet de steppermotor uit
+    Serial.println("Stepper disabled after timeout");
+  }
+}
+
+/*######################################################### Loop ################################################################*/
+
 void loop() {
   /*
   val = (analogRead(analogPin) );  // read the input pin
@@ -122,57 +149,21 @@ void loop() {
 
   if (val >= 0.0 && val <= 0.4) { //Nothing
   } else if (val >= 0.5 && val <= 1.0) { //Open Camera Gripper
-    ServoCameraOpen(30);
+    ServoCamera(30);
   } else if (val >= 1.1 && val <= 1.5) { //Close Camera Gripper
-    ServoCameraClose(89);
-  } else if (val >= 1.6 && val <= 2.0) { //Extend Actuator 1
-    if (digitalRead(endswitchPin) == LOW) {
-      ExtendAct(1);
-    }
-    else {
-      StilstandAct(1);
-    }
-  } else if (val >= 2.1 && val <= 2.5) { //Retract Actuator 1
-    if (digitalRead(endswitchPin) == LOW) {
-      RetractAct(1);
-    }
-    else {
-      StilstandAct(1);
-    }
-  } else if (val >= 2.6 && val <= 3.0) { //Extend Actuator 2
+    ServoCamera(89);
+  } else if (val >= 1.6 && val <= 2.0) { //Slightly Open Camera Gripper
+    ServoCamera(85);
+  } else if (val >= 2.1 && val <= 2.5) { //Extend Actuator 1
+    if (digitalRead(endswitchPin) == LOW) {ExtendAct(1);}       else {StilstandAct(1);}
+  } else if (val >= 2.6 && val <= 3.0) { //Retract Actuator 1
+    if (digitalRead(endswitchPin) == LOW) {RetractAct(1);}      else {StilstandAct(1);}
+  } else if (val >= 3.1 && val <= 3.5) { //Extend Actuator 2
     ExtendAct(2);
-  } else if (val >= 3.1 && val <= 3.5) { //Retract Actuator 2
+  } else if (val >= 3.6 && val <= 4.0) { //Retract Actuator 2
     RetractAct(2);
-  } else if (val >= 3.6 && val <= 4.0) { //Activate glue
-  
-   if (Serial.available() > 0) {
-        // Read the number of steps from the serial input
-        long steps = Serial.parseInt();
-        last_message_time = millis();
-        // Check if steps value is valid (not 0)
-        if (steps != 0) {
-            // Move the stepper by the given number of steps
-            stepper.move(steps);
-            stepper.runToPosition();  // Block until the move is completed
-
-            // Send a "Done" message to the PC after the movement is completed
-            delay(1);
-            Serial.println("Done");
-        if (steps == 0){Serial.println("Done");}    
-        }
-    }
-    if (millis() - last_message_time > 10 * 1000) {
-  // Serial.println("off");
-      digitalWrite(X_ENABLE_PIN, HIGH);
-        }
-
-
-
-
-  } else if (val >= 4.1 && val <= 4.5) {
-
-  } else if (val >= 4.6 && val <= 5.0) {
-
-  }
+  } else if (val >= 4.1 && val <= 4.5) { //Activate glue
+    GlueStation(320);
+  } else if (val >= 4.6 && val <= 5.0) {}
   val = 0.0;
 }
